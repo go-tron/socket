f0420ddc58c3f76e9abb124f9937a4e8e975eb33
zhangyi <zh1@vip.qq.com>
2023-12-15 01:36:42 +0800

    1

--- clientList.go
+++ clientList.go
@@ -2,16 +2,24 @@ package socket
 
 import (
 	"slices"
+	"sync"
 )
+
+var clientMu sync.Mutex
 
 type ClientList []*Client
 
 func (cl *ClientList) RemoveByConnectId(connectId string) {
+	clientMu.Lock()
+	defer clientMu.Unlock()
 	*cl = slices.DeleteFunc(*cl, func(i *Client) bool {
 		return connectId == i.Conn.ID()
 	})
 }
+
 func (cl *ClientList) Add(c *Client) {
+	clientMu.Lock()
+	defer clientMu.Unlock()
 	*cl = append(*cl, c)
 }
 
